4bc1002fdbf46625a9ab939878cfc9591be711c8
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-10-30 05:38:17 +0530

    first commit

---
+++ src/Array_Programs/PrintArray.java
@@ -0,0 +1,10 @@
+package Array_Programs;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 5, 3, 6, 7};
+        for(int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
