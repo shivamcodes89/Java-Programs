a3a146777a69c5ccf00c29f098a52903fe371dd1
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-11-01 08:32:19 +0530

    code

---
+++ src/Array_Programs/CountDuplicateElement.java
@@ -0,0 +1,28 @@
+package Array_Programs;
+
+public class CountDuplicateElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 10, 2, 1, 6, 2, 6, 2, 1, 7, 5, 7, 9, 8};
+        int[] freq = new int[arr.length];
+        int visited = -1;
+        int total = 0;
+        for(int i=0; i<arr.length; i++){
+            int count = 1;
+            for(int j=1+i; j<arr.length; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                    freq[j] = visited;
+                }
+            }
+            if(freq[i] != visited){
+                freq[i] = count;
+            }
+        }
+        for(int i=0; i<freq.length; i++){
+            if(freq[i] != visited && freq[i] != 1){
+                total++;
+            }
+        }
+        System.out.println(total);
+    }
+}
