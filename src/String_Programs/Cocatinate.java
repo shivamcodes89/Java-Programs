2de03535765606f51953a8a482c285deb216a3d9
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-11-16 16:38:52 +0530

    code

---
+++ src/String_Programs/Cocatinate.java
@@ -0,0 +1,10 @@
+package String_Programs;
+
+public class Cocatinate {
+    public static void main(String[] args) {
+        String a = "Arjit";
+        String b = "Rajput";
+        String c = a.concat(" ").concat(b);
+        System.out.println(c);
+    }
+}
