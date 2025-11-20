e33e396e806f43b57c5d78e81d7028b17b741062
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-11-20 20:44:45 +0530

    code

---
+++ src/ElementaryCodingSheet2/PalindromePossible.java
@@ -0,0 +1,42 @@
+package ElementaryCodingSheet2;
+
+import java.util.Scanner;
+
+public class PalindromePossible {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //1. Agar total digits EVEN ho -> koi bhi digit odd frequency ka nahi hona chahiye.
+        //2. Agar total digits ODD ho -> sirf 1 digit odd frequency ka allowed hai.
+        System.out.print("enter number: ");
+        int num = sc.nextInt();
+        int[] fre = new int[10];
+        int totalDigit = 0;
+        int num1 = num;
+        int num2 = 0;
+        while(num > 0){
+            int digit = num % 10;
+            fre[digit]++;
+            totalDigit++;
+            num = num / 10;
+        }
+        for(int i = 0; i < 10; i++){
+            if(fre[i] % 2 != 0){
+                num2++;
+            }
+        }
+        if(totalDigit % 2 == 0){
+            if(num2 == 0){
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+        else {
+            if(num2 == 1){
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+    }
+}
