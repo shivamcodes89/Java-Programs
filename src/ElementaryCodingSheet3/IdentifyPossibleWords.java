ef871f065ae677b9a2128acdc7adda19d7a943fc
Shivam Rajput <rajputshivam8550855@gmail.com>
2025-12-02 02:03:19 +0530

    code

--- src/ElementaryCodingSheet3/IdentifyPossibleWords.java
+++ src/ElementaryCodingSheet3/IdentifyPossibleWords.java
@@ -1,22 +1,42 @@
-package ElementaryCodingSheet3;
-
 public class IdentifyPossibleWords {
     public static void main(String[] args) {
         String input1 = "Fi_er";
         String input2 = "Fever:filer:Filter:Fixer:fiber:fibre:tailor:offer";
+
         String[] arr = input2.split(":");
         input1 = input1.toLowerCase();
-        int index = input1.indexOf("_");//output -> filer, Fixer, fiber
-        int length1 = input1.length();
+        String result = "";
 
         for(String word : arr){
             word = word.toLowerCase();
-            int length2 = word.length();
-            for(int i=0; i<arr.length; i++){
-                if(length1 == length2){
+            boolean flag = true;
 
+            if(input1.length() != word.length()){
+                continue;
+            }
+
+            for(int i=0; i<word.length(); i++){
+                char a = input1.charAt(i);
+                char b = word.charAt(i);
+
+                if(a == '_') continue;   // allow anything
+
+                if(a != b){
+                    flag = false;
+                    break;
                 }
             }
+
+            if(flag){
+                if(!result.isEmpty()) result += ":";
+                result += word.toUpperCase();
+            }
         }
+
+        if(result.isEmpty()){
+            result = "ERROR-009";
+        }
+
+        System.out.println(result);
     }
 }
