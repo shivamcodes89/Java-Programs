9c60709c0b4d18a7085bde95830bbe0bfc3ca984
Shivam Rajput <rajputshivam8550855@gmail.com>
2026-01-20 21:46:35 +0530

    code

---
+++ src/String_Programs/ReverseString.java
@@ -0,0 +1,19 @@
+package String_Programs;
+
+public class ReverseString {
+    public static void main(String[] args) {
+//        String s = "Hello";
+//        String s1 = "";
+//        for(int i=s.length()-1; i>=0; i--){
+//            char ch = s.charAt(i);
+//            s1 = s1 + ch;
+//        }
+//        System.out.println(s1);
+
+        String s = "Hello";
+        StringBuilder sb = new StringBuilder();
+        for(int i=s.length()-1; i>=0; i--){
+
+        }
+    }
+}
